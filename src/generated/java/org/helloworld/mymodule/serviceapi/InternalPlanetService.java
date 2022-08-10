55bc7e2f4efedf35dae45c1a9331fdd4f5ec74fe
felippe97 <84409435+felippe97@users.noreply.github.com>
2022-08-10 16:48:05 +0200

    Update InternalPlanetService.java

--- src/generated/java/org/helloworld/mymodule/serviceapi/InternalPlanetService.java
+++ src/generated/java/org/helloworld/mymodule/serviceapi/InternalPlanetService.java
@@ -3,6 +3,7 @@ package org.helloworld.mymodule.serviceapi;
 import java.util.List;
 import org.helloworld.mymodule.domain.Planet;
 import org.helloworld.mymodule.exception.PlanetNotFoundException;
+import org.sculptor.framework.accessapi.ConditionalCriteria;
 import org.sculptor.framework.context.ServiceContext;
 
 /**
@@ -15,6 +16,8 @@ public interface InternalPlanetService {
 	public Planet findById(ServiceContext ctx, Long id) throws PlanetNotFoundException;
 
 	public List<Planet> findAll(ServiceContext ctx);
+
+	public List<Planet> findByCondition(ServiceContext ctx, List<ConditionalCriteria> condition);
 
 	public Planet save(ServiceContext ctx, Planet entity);
 
