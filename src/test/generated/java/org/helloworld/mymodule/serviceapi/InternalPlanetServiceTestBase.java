a6feed1502af02cb5309af5b5e93f90e37ff567b
felippe97 <84409435+felippe97@users.noreply.github.com>
2022-08-10 16:48:19 +0200

    Update InternalPlanetServiceTestBase.java

--- src/test/generated/java/org/helloworld/mymodule/serviceapi/InternalPlanetServiceTestBase.java
+++ src/test/generated/java/org/helloworld/mymodule/serviceapi/InternalPlanetServiceTestBase.java
@@ -9,6 +9,8 @@ public interface InternalPlanetServiceTestBase {
 
 	public void testFindAll() throws Exception;
 
+	public void testFindByCondition() throws Exception;
+
 	public void testSave() throws Exception;
 
 	public void testDelete() throws Exception;
