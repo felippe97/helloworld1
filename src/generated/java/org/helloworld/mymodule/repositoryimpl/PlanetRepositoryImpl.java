bdb4728f64175d70e84628ab9779128593fd6634
felippe97 <84409435+felippe97@users.noreply.github.com>
2022-08-12 12:38:25 +0200

    Update PlanetRepositoryImpl.java

--- src/generated/java/org/helloworld/mymodule/repositoryimpl/PlanetRepositoryImpl.java
+++ src/generated/java/org/helloworld/mymodule/repositoryimpl/PlanetRepositoryImpl.java
@@ -6,15 +6,12 @@ import javax.persistence.PersistenceContext;
 import org.helloworld.mymodule.domain.Planet;
 import org.helloworld.mymodule.domain.PlanetRepository;
 import org.helloworld.mymodule.exception.PlanetNotFoundException;
-import org.sculptor.framework.accessapi.ConditionalCriteria;
 import org.sculptor.framework.accessapi.DeleteAccess;
 import org.sculptor.framework.accessapi.FindAllAccess2;
-import org.sculptor.framework.accessapi.FindByConditionAccess2;
 import org.sculptor.framework.accessapi.FindByIdAccess;
 import org.sculptor.framework.accessapi.SaveAccess;
 import org.sculptor.framework.accessimpl.jpa.JpaDeleteAccessImpl;
 import org.sculptor.framework.accessimpl.jpa.JpaFindAllAccessImplGeneric;
-import org.sculptor.framework.accessimpl.jpa.JpaFindByConditionAccessImplGeneric;
 import org.sculptor.framework.accessimpl.jpa.JpaFindByIdAccessImpl;
 import org.sculptor.framework.accessimpl.jpa.JpaSaveAccessImpl;
 import org.springframework.stereotype.Repository;
@@ -81,22 +78,6 @@ public class PlanetRepositoryImpl implements PlanetRepository {
 		return ao.getResult();
 	}
 
-	/**
-	 * Delegates to {@link org.sculptor.framework.accessapi.FindByConditionAccess}
-	 */
-	public List<Planet> findByCondition(List<ConditionalCriteria> condition) {
-		return findByCondition(condition, getPersistentClass());
-	}
-
-	public <R> List<R> findByCondition(List<ConditionalCriteria> condition, Class<R> resultType) {
-
-		FindByConditionAccess2<R> ao = createFindByConditionAccess(resultType);
-
-		ao.setCondition(condition);
-		ao.execute();
-		return ao.getResult();
-	}
-
 	@PersistenceContext(unitName = "MyAppEntityManagerFactory")
 	private EntityManager entityManager;
 
@@ -142,22 +123,6 @@ public class PlanetRepositoryImpl implements PlanetRepository {
 
 	protected <T, R> FindAllAccess2<R> createFindAllAccess(Class<T> type, Class<R> resultType) {
 		JpaFindAllAccessImplGeneric<T, R> ao = new JpaFindAllAccessImplGeneric<T, R>(type, resultType);
-		ao.setEntityManager(getEntityManager());
-		return ao;
-	}
-
-	// convenience method
-	protected FindByConditionAccess2<Planet> createFindByConditionAccess() {
-		return createFindByConditionAccess(getPersistentClass(), getPersistentClass());
-	}
-
-	// convenience method
-	protected <R> FindByConditionAccess2<R> createFindByConditionAccess(Class<R> resultType) {
-		return createFindByConditionAccess(getPersistentClass(), resultType);
-	}
-
-	protected <T, R> FindByConditionAccess2<R> createFindByConditionAccess(Class<T> type, Class<R> resultType) {
-		JpaFindByConditionAccessImplGeneric<T, R> ao = new JpaFindByConditionAccessImplGeneric<T, R>(type, resultType);
 		ao.setEntityManager(getEntityManager());
 		return ao;
 	}
